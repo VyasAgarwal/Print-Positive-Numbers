07fe02c379348f78a843e664694157f1fcb5dc94
VyasAgarwal <78445380+VyasAgarwal@users.noreply.github.com>
2021-02-10 23:26:52 +0530

    Add files via upload

---
+++ Print positive number.py
@@ -0,0 +1,11 @@
+list1 = [12, -7, 5, 64, -14]
+print ("Input: List1 = ", list1)
+for x in list1:
+    if x>0:
+        print(x, end = " ")
+
+list2 = [12, 14, -95, 3]
+print ("\nInput: List2 = ", list2)
+for y in list2:
+    if y>0:
+        print([y], end = " ")
